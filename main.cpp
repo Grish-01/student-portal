a99338832ecd9e41db328c29def17a34400de66d
Grish-01 <grishma.trivedi@students.iiit.ac.in>
2026-08-15 17:21:24 +0530

    feat: add user greeting function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <string>
 
 void printWelcomeMessage() {
     std::cout << "      Welcome to Git !       " << std::endl;
 }
 
+void greetUser() {
+    std::cout << "Hello, Developer!" << std::endl;
+}
+
 int main() {
     printWelcomeMessage();
+    greetUser();
     return 0;
 }
